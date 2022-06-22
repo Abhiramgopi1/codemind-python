1966fd6f654c7d002d6338ed9e320296b01bb657
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-06-22 19:36:14 +0530

    n=list(map(str,input().split())) c=0 d=[] for i in n:     for j in i:         if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':             c+=1         elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':             c+=1     d.append(c)     c=0 x=min(d) print(d.count(x))

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,13 @@
+n=list(map(str,input().split()))
+c=0
+d=[]
+for i in n:
+    for j in i:
+        if j=='a' or j=='e' or j=='i' or j=='o' or j=='u':
+            c+=1
+        elif j=='A' or j=='E' or j=='I' or j=='O' or j=='U':
+            c+=1
+    d.append(c)
+    c=0
+x=min(d)
+print(d.count(x))
