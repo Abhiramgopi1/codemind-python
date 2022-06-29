714225b4b86ea6654b764f3866324263bbea1d4e
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-06-29 22:27:51 +0530

    x = input() a = len(x) y = str(int(x)**2) b = len(y) s='' for i in range(b-a,b,1):     s+=y[i] if x==s:     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,8 +1,11 @@
-n=int(input())
-q=n*n
-c=len(str(n))
-d=q%(10**c)
-if d==n:
+x = input()
+a = len(x)
+y = str(int(x)**2)
+b = len(y)
+s=''
+for i in range(b-a,b,1):
+    s+=y[i]
+if x==s:
     print('Automorphic Number')
 else:
     print('Not an Automorphic Number')
