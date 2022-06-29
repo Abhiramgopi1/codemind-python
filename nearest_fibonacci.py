0ee7cd08bccec4c119c1062d1ccc0158e897b19b
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-06-29 20:47:13 +0530

    def is_fib(j):     a = 0     b = 1     c = a+b     while c<j:         c = a + b         a = b         b = c     if c == j:         return j
    n = int(input())
    k=n
    for j in range(n,0,-1):
        if is_fib(j):
            a = j
            break
    while k!=0:
        if is_fib(k):
            b = k
            break
        k+=1
    if (n-a)<(b-n):
        print(a)
    elif (n-a)==(b-n):
        print(a,b)
    else:
        print(b)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,28 +1,29 @@
-def fib(i):
-    a=0
-    b=1
-    c=a+b
-    while c<i:
-        c=a+b
-        a=b
-        b=c
-    if c==i:
-        return i
-        
-x=int(input())
-k=x
-for i in range(x,0,-1):
-    if fib(i):
-        a=i
+def is_fib(j):
+    a = 0
+    b = 1
+    c = a+b
+    while c<j:
+        c = a + b
+        a = b
+        b = c
+    if c == j:
+        return j
+
+
+n = int(input())
+k=n
+for j in range(n,0,-1):
+    if is_fib(j):
+        a = j
         break
 while k!=0:
-    if fib(k):
-        b=k
+    if is_fib(k):
+        b = k
         break
     k+=1
-if (x-a)<(b-x):
+if (n-a)<(b-n):
     print(a)
-elif (x-a)==(b-x):
+elif (n-a)==(b-n):
     print(a,b)
 else:
     print(b)
