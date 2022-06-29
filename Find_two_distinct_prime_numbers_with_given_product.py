0204a00b16eca94959f7182845c9787b2c94e7b6
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-06-29 23:00:53 +0530

    def prime(i):     if i==1:         return 0     for j in range(2,i//2):         if i%j==0:             return 0     return 1
    x=int(input())
    c=0
    for i in range(1,x):
         if x%i==0:
            if prime(i):
                print(i,end=" ")
                c+=1
    if c==0:
        print('-1')

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -5,12 +5,14 @@ def prime(i):
         if i%j==0:
             return 0
     return 1
+    
 x=int(input())
 c=0
 for i in range(1,x):
-    if x%i==0:
+     if x%i==0:
         if prime(i):
             print(i,end=" ")
             c+=1
 if c==0:
     print('-1')
+    
