e5410c74cc474b4cb413daf54b4bb6776809c84b
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-09-06 21:08:27 +0530

    def prime(i):     if i==1:         return 0     for j in range(2,int(i**0.5)+1):         if i%j==0:             return 0     return 1 n=int(input()) l=list(map(int,input().split())) c=0 k=int(input()) for i in l:     if i<=k:         if prime(i):             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,16 @@
+def prime(i):
+    if i==1:
+        return 0
+    for j in range(2,int(i**0.5)+1):
+        if i%j==0:
+            return 0
+    return 1
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+k=int(input())
+for i in l:
+    if i<=k:
+        if prime(i):
+            c+=1
+print(c)
